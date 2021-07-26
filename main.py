a151e423422ea0b46d1bdf9cedf0dec08a22bd5d
MrRobot0ne <drebin341@gmail.com>
2021-07-26 22:05:25 +0300

    IMPORT,  WHILE TRUE

--- main.py
+++ main.py
@@ -1,24 +1,29 @@
 import json
 import time
-import telebot
 import requests
-
+import telebot
 from bs4 import BeautifulSoup
 
 with open("config.json") as file:
     config = json.load(file)
+
 bot = telebot.TeleBott(config['API_TOKEN'])
 page = 1
 
-while true
-    domain =
-    responce =
-    all_image = BeautifulSoup(responce, 'lxml').find_all('div', class='position')
+while True:
+    domain = f"https://{page}"
+    responce = requests.get(domain).text
+    all_image = BeautifulSoup(responce, 'lxml').find_all('div', class_="position-relative")
 
     for image int all_image:
         stroage_url = image.find('a').get('href')
 
-        if storage_url != '#':
-            image = requests.get(stroge_url).text
-            result_link
-            image_bytes = requests.get(f"https://{result_link}").content
+    if storage_url != '#':
+        image = requests.get(stroge_url).text
+    result_link = BeautifulSoup(image, 'lxml').find('div', id='image_wrapper').find('img').get('src')
+    image_bytes = requests.get(f"https://{result_link}").content
+
+    for channel in config['CHANNEL_LOGIN']:
+        bot.send_photo(channel, image_byes)
+        time.sleep(int(config['SLEEP']))
+    page += 1
